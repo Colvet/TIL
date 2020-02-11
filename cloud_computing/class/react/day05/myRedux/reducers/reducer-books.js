c469fffc53588022addee737ffcaf5d69bd13efc
SHINHAK KIM <hicybersole004@gmail.com>
2020-02-11 17:51:18 +0900

    200211

---
+++ cloud_computing/class/react/day05/myRedux/reducers/reducer-books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return[
+        {title: 'Javascript', page: '101'},
+        {title: 'Docker', page: '102'},
+        {title: 'Java', page: '103'},
+        {title: 'MSA', page: '104'}
+    ]
+}

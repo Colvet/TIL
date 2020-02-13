80646b221cfc5e0a02577194ffd7d53806b3744f
SHINHAK KIM <hicybersole004@gmail.com>
2020-02-13 17:20:19 +0900

    edit

--- cloud_computing/class/react/day06/weather/actions/index.js
+++ cloud_computing/class/react/day06/weather/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '50ed77cb5232eae172df470625b34154';
+const API_KEY = 'APIKEY';
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
